db25c388c712ffb86f4ce001537bb96184f2be78
DehamiDiv <dehamidivyanjali166@gmail.com>
2026-07-16 16:49:28 +0530

    feat: add movie routes and configure ES modules

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-const express = require("express");
+import express from "express";
+
+//import Routes
+import movieRoutes from "./routes/movieRoutes.js";
 
 const app = express();
+
+//API ROUTES
+app.use("/movies", movieRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 const PORT = 5001;
 
 const server = app.listen(PORT, () => {
