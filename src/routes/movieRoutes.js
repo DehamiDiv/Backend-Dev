254e14abbb5ff53869dc2ff0e5360a97abee91dd
DehamiDiv <dehamidivyanjali166@gmail.com>
2026-07-16 17:13:18 +0530

    feat: initialize Prisma ORM with schema configuration and dependencies

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
-  res.send("Hello from movies");
+router.get("/", (req, res) => {
+  res.json("Hello from movies");
+});
+router.post("/", (req, res) => {
+  res.json("Movie Added");
+});
+router.put("/", (req, res) => {
+  res.json("Movie Updated");
+});
+router.delete("/", (req, res) => {
+  res.json("Movie Deleted");
 });
 
 export default router;
