dc921194d0e9b522a4cae3d69da884ea6300823a
DehamiDiv <dehamidivyanjali166@gmail.com>
2026-07-19 22:33:52 +0530

    feat: initialize Prisma ORM with User model, database configuration, and dotenv support

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import { config } from "dotenv";
+
+config();
+connectDB();
 
 //import Routes
 import movieRoutes from "./routes/movieRoutes.js";
@@ -15,4 +20,30 @@ const PORT = 5001;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+//Handle unhandeled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//handle uncaught expectations
+process.on("uncaughtException", (err) => {
+  console.error(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//Graceful shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
 });
